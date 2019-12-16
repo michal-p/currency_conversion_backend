f2bf155897d8f6aeb76b5715ef724f2311fff0b4
Michal Púchy <puchy.michal@gmail.com>
2019-12-16 14:12:18 +0100

    Backend services to take data from outside api, enviroment variables

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
+require('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const currencyServices = require('./services/currencies').default
+const cors = require('cors')
+const morgan = require('morgan')
 
-let notes = []
+app.use(express.static('build')) //Frontend files
+app.use(bodyParser.json())
+app.use(cors())
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+let currenciesData = {}
+
+app.get('/api/currencies', (request, response) => {
+  console.log('server bub')
+  currencyServices
+    .getCurrencies()
+    .then(currencies => {
+      currenciesData = currencies
+      console.log('tot: ', currenciesData)
+      response.json(currenciesData) //The toJSON method we defined transforms object into a string just to be safe.
+    })
+
 })
 
-app.get('/notes', (req, res) => {
-  res.json(notes)
-})
-
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`)
 })
---
+++ services/currencies.js
@@ -0,0 +1,31 @@
+const axios = require('axios')
+const baseUrl = 'https://openexchangerates.org/api/'
+const apiID = process.env.API_KEY
+const endPoints = {
+  latest: 'latest.json',
+  currencies: 'currencies.json'
+}
+const createUrl = (action) => {
+  return `${baseUrl}/${action}/?app_id=${apiID}`
+}
+
+const getLatest = () => {
+  const request = axios.get(createUrl(endPoints.latest))
+  return request.then(response => {
+    console.log('getAll promise fulfilled')
+    return response.data
+  })
+}
+
+const getCurrencies = () => {
+  const request = axios.get(createUrl(endPoints.currencies))
+  return request.then(response => {
+    console.log('getAll promise fulfilled')
+    return response.data
+  })
+}
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+})
+exports.default = { getLatest, getCurrencies }
