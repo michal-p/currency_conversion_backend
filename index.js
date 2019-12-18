421a09dcb11d5c3192977237ce33a13d3f256c62
Michal Púchy <puchy.michal@gmail.com>
2019-12-18 15:28:11 +0100

    Error handling and statistics send on load

--- index.js
+++ index.js
@@ -20,24 +20,24 @@ let currenciesLatest = {}
 
 const convert = (amount, ratesFrom, ratesTo) => {
   const amountTo = Math.abs(amount) * ratesTo / ratesFrom
-  return Number(amountTo.toFixed(2))
+  return Number(amountTo.toFixed(6))
 }
 
-app.get('/api/currencies', (request, response) => {
+app.get('/api/currencies', (request, response, next) => {
   console.log('server currencies')
   currencyServices
     .getCurrencies()
     .then(currencies => {
       currenciesList = currencies
-      console.log('tot: ', currenciesList)
+      // console.log('tot: ', currenciesList)
       response.json(currenciesList) //The toJSON method we defined transforms object into a string just to be safe.
     }).catch(error => {
       console.log('currencies - getCurrencies data service error: ', error)
+      next(error)
     })
-
 })
 
-app.get('/api/latest', (request, response) => {
+app.get('/api/latest', (request, response, next) => {
   console.log('server latest')
   currencyServices
     .getLatest()
@@ -47,8 +47,45 @@ app.get('/api/latest', (request, response) => {
       response.json(currenciesLatest) //The toJSON method we defined transforms object into a string just to be safe.
     }).catch(error => {
       console.log('latest - getLatest data service error: ', error)
+      next(error)
     })
+})
 
+app.get('/api/statistics', (request, response, next) => {
+  console.log('server statistics')
+  let obj = {}
+  Transfer.countDocuments()
+    .then(result => {
+      console.log('Transfer count() result: ', result)
+      obj.amountRequests = result
+      Transfer.aggregate(
+        [{ $group: {
+          _id: null,
+          total: { $sum: '$convertedInDollars' } } }]
+      ).then(result => {
+        console.log('amountConvertedInUSD: ', result)
+        obj.amountConvertedInUSD = result[0].total
+      })
+      Transfer.aggregate(
+        [{ $group:{
+          _id: '$currencyTo',
+          // totalAmount: { $sum: '$convertedInDollars' },
+          count: { $sum: 1 }
+        } },
+        { $sort:{
+          'count': -1
+        } },
+        { $limit: 1 }]
+      ).then(result => {
+        obj.popular = result[0]._id
+        console.log('Transfer aggregate result: ', result)
+        response.json(obj)
+      }).catch(error => next(error))
+    })
+    .catch(error => {
+      console.log('statistics - getStatistics data service error: ', error)
+      next(error)
+    })
 })
 
 app.post('/api/convert', (request, response, next) => {
@@ -57,7 +94,7 @@ app.post('/api/convert', (request, response, next) => {
   console.log('body: ', body)
   const transfer = new Transfer({
     currencyFrom: body.fromCurrency,
-    amountFrom: Number(Number(body.fromAmount).toFixed(2)),
+    amountFrom: Number(Number(body.fromAmount).toFixed(6)),
     currencyTo: body.toCurrency
   })
 
@@ -72,20 +109,41 @@ app.post('/api/convert', (request, response, next) => {
         currenciesLatest.rates[transfer.currencyTo]
       )
       transfer.convertedInDollars = Number(
-        (transfer.amountFrom / currenciesLatest.rates[transfer.currencyFrom]).toFixed(2)
+        (transfer.amountFrom / currenciesLatest.rates[transfer.currencyFrom]).toFixed(6)
       )
       transfer.date = new Date()
-      console.log('transfer: ', transfer)
       transfer.save()
         .then(newTransfer => newTransfer.toJSON())
-        .then(newTransferFormatted => response.json(newTransferFormatted))
+        .then(newTransferFormatted => {
+          console.log('newTransfer from db: ', newTransferFormatted)
+          response.json(newTransferFormatted)
+        })
         .catch(error => next(error))
     })
     .catch(error => {
       console.log('convert - getLatest service error: ', error)
+      next(error)
     })
 
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error("Deafault custom error Handler: ", error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+// default custom Express error handler
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
